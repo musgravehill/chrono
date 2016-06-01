d3f216d5e4156d564a6d3937ab464b6ebadea16e
Boris K.V <musgravehill@gmail.com>
2016-06-01 20:23:09 +0300

    v3

--- v3-16mhz-3.3v/v3-16mhz-3.3v.ino
+++ v3-16mhz-3.3v/v3-16mhz-3.3v.ino
@@ -1,21 +1,3 @@
-/*********************************************************************
-This is an example sketch for our Monochrome Nokia 5110 LCD Displays
-
-  Pick one up today in the adafruit shop!
-  ------> http://www.adafruit.com/products/338
-
-These displays use SPI to communicate, 4 or 5 pins are required to
-interface
-
-Adafruit invests time and resources providing this open source code,
-please support Adafruit and open-source hardware by purchasing
-products from Adafruit!
-
-Written by Limor Fried/Ladyada  for Adafruit Industries.
-BSD license, check license.txt for more information
-All text above, and the splash screen must be included in any redistribution
-*********************************************************************/
-
 #include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_PCD8544.h>
@@ -31,7 +13,7 @@ Adafruit_PCD8544 display = Adafruit_PCD8544(9, 8, 7, 6, 5);
 void setup()   {
   Serial.begin(9600);
   display.begin();
-  display.setContrast(80); 
+  display.setContrast(70); 
   display.clearDisplay(); 
  
   display.setTextSize(1);
